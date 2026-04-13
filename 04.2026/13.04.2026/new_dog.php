d543017b58432476515ad354fc225c73e7177c28
DawidOrdon <dawid.ordon2000@gmail.com>
2026-04-13 14:41:49 +0200

    .

--- 04.2026/13.04.2026/new_dog.php
+++ 04.2026/13.04.2026/new_dog.php
@@ -1,4 +1,8 @@
 <?php
 session_start();
 $db = new mysqli('localhost', 'root', '', 'komisarjat');
+$sql = "INSERT INTO komisarjat.psy (imie, rasa_id, waga, wzrost, data_narodzin, box_id) VALUES (?, ?, ?, ?, ?, ?);";
+$query = $db->prepare($sql);
+$query->bind_param('siddsi',$_POST['']);
+$query->execute();
 var_dump($_POST);

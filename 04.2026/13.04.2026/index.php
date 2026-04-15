00e54bb4b553c5e018856af47ad2d98dc8e652b4
DawidOrdon <dawid.ordon2000@gmail.com>
2026-04-15 09:47:05 +0200

    .

--- 04.2026/13.04.2026/index.php
+++ 04.2026/13.04.2026/index.php
@@ -18,6 +18,12 @@ $db = new mysqli('localhost', 'root', '', 'komisarjat');
         select{
             width: 208px;
         }
+        td, th{
+            border:1px solid black;
+        }
+        table{
+            border-collapse: collapse;
+        }
     </style>
 </head>
 <body>
@@ -25,7 +31,7 @@ $db = new mysqli('localhost', 'root', '', 'komisarjat');
     <table>
         <tr>
             <td><label for="imie">Imie psa:</label></td>
-            <td><input type="text" name="imie" id="imie"></td>
+            <td><input type="text" name="imie" id="imie" value="<?php echo isset($_SESSION['imie'])?$_SESSION['imie'] : " " ?>"></td>
         </tr>
         <tr>
             <td><label for="rasa">Rasa psa:</label></td>
@@ -36,7 +42,8 @@ $db = new mysqli('localhost', 'root', '', 'komisarjat');
                         $query->execute();
                         $result = $query->get_result();
                         while($row = $result->fetch_object()) {
-                            echo "<option value='" . $row->rasa_id . "'>" . $row->nazwa . "</option>";
+                            $selected = $_SESSION['rasa'] == $row->rasa_id?"selected":"";
+                            echo "<option value='" . $row->rasa_id . "' $selected>" . $row->nazwa . "</option>";
                         }
                     ?>
                 </select>
@@ -44,15 +51,15 @@ $db = new mysqli('localhost', 'root', '', 'komisarjat');
         </tr>
         <tr>
             <td><label for="waga">Waga psa:</label></td>
-            <td><input type="number" name="waga" step="0.01" id="waga"></td>
+            <td><input type="number" name="waga" step="0.01" id="waga" value="<?php echo isset($_SESSION['waga'])?$_SESSION['waga'] : " " ?>"></td>
         </tr>
         <tr>
             <td><label for="wzrost">Wzrost psa:</label></td>
-            <td><input type="number" name="wzrost" step="0.01" id="wzrost"></td>
+            <td><input type="number" name="wzrost" step="0.01" id="wzrost" value="<?php echo isset($_SESSION['wzrost'])?$_SESSION['wzrost'] : " " ?>"></td>
         </tr>
         <tr>
             <td><label for="data">Data narodzin psa:</label></td>
-            <td><input type="date" name="data" step="0.01" id="data"></td>
+            <td><input type="date" name="data" step="0.01" id="data" value="<?php echo isset($_SESSION['data'])?$_SESSION['data'] : " " ?>"></td>
         </tr>
         <tr>
             <td><label for="box">Box</label></td>
@@ -63,7 +70,8 @@ $db = new mysqli('localhost', 'root', '', 'komisarjat');
                         $query->execute();
                         $result = $query->get_result();
                         while ($row = $result->fetch_object()) {
-                            echo "<option value='" . $row->box_id . "'>" . $row->typ . " max:{$row->max_wielkosc} wolne:{$row->wolne}</option>";
+                            $selected = $_SESSION['box'] == $row->box_id ? "selected" : "";
+                            echo "<option value='" . $row->box_id . "' $selected>" . $row->typ . " max:{$row->max_wielkosc} wolne:{$row->wolne}</option>";
                         }
                     ?>
                 </select></td>
@@ -84,9 +92,40 @@ $db = new mysqli('localhost', 'root', '', 'komisarjat');
                 unset($_SESSION['message']);
             }
         ?>
-
-
     </table>
 </form>
+<table>
+    <tr>
+        <th>id</th>
+        <th>imie</th>
+        <th>rasa</th>
+        <th>waga</th>
+        <th>wzrost</th>
+        <th>narodziny</th>
+        <th>box</th>
+        <th>usun</th>
+    </tr>
+<?php
+    $sql = "select p.pies_id as id, p.imie as imie, p.waga as waga, p.wzrost as wzrost, p.data_narodzin as narodziny,  b.typ as box_typ, b.max_wielkosc as wielkosc, b.wolne as wolne, r.nazwa as rasa from psy as p join rasa as r on p.rasa_id = r.rasa_id join boxy as b on p.box_id = b.box_id order by p.pies_id";
+    $query = $db->prepare($sql);
+    $query->execute();
+    $result = $query->get_result();
+    while ($row = $result->fetch_object()){
+        echo "<tr>";
+        echo "<td>{$row->id}</td>";
+        echo "<td>{$row->imie}</td>";
+        echo "<td>{$row->rasa}</td>";
+        echo "<td>{$row->waga}</td>";
+        echo "<td>{$row->wzrost}</td>";
+        echo "<td>{$row->narodziny}</td>";
+        echo "<td>{$row->box_typ} max:{$row->wielkosc} wolne:{$row->wolne}</td>";
+        echo "<td><a href='delete.php?id={$row->id}'><button>usuń</button></a></td>";
+        echo "</tr>";
+    }
+
+
+
+?>
+</table>
 </body>
 </html>
